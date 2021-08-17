4d51da5e99ed7381cfb21be4d6db892a9e573264
unknown <paschoaloliveira@hotmail.com>
2021-08-17 16:31:31 -0300

    alteração em empregados

--- js/empregados.js
+++ js/empregados.js
@@ -1,9 +1,34 @@
-function resgataEmpregados(){  
+let mustacheTemplate
+$(window).on('load', () => {
+  mustacheTemplate = document.getElementById('tableClientes').innerHTML;
+  mustacheTemplatePaginacao = document.getElementById('paginacao').innerHTML;
+})
+
+function resgataEmpregados(numeroPagina){  
         
+    numeroPagina--;
+    var nome = $('#txtNome').val();
+    var cpf = $('#txtCpf').val();
+    var nomeAgencia = $('#txtNomeAgencia').val();
+
+    var dataObj = {};
+
+    if(nome != ""){
+        dataObj = {...dataObj, ["nome"] : nome}
+    }
+    if(cpf != ""){
+        dataObj = {...dataObj, ["cpf"] : cpf};
+    }
+    if(nomeAgencia != ""){
+        dataObj = {...dataObj, ["nomeAgencia"] : nomeAgencia};
+    }
+
+    //console.log(dataObj);
     $.ajax
     ({
         type: "GET",
-        url: "http://localhost:8090/v1/empregados",
+        url: "http://localhost:8090/v1/empregados?numeroPagina=" + numeroPagina,
+        data:dataObj,
         dataType: 'json',
         beforeSend: function (request) {
             request.setRequestHeader('Authorization', make_base_auth());
@@ -11,25 +36,29 @@ function resgataEmpregados(){
         success: function (data){
             console.log(data);
             var paginaAtual = data['number'];
-            var totalPages = data['totalPages'];
             
             $('#tableClientes').css("visibility", "visible");
             
-            var tableHtml = document.getElementById('mustache').innerHTML;
-            var rendered = Mustache.render(tableHtml, data);
-            $('#mustache').html(rendered);
+            //var tableHtml = document.getElementById('tableClientes').innerHTML;
+            var rendered = Mustache.render(mustacheTemplate, data);
+            $('#tableClientes').html(rendered);
             
-            
-            //$('#tableClientes').DataTable();
-            /*
-            $.each(data['content'], function (key, item) {             
-                $('#tableClientes').append('<tr><td>' + item.cpf + '</td> <td>' + 
-                                                        item.nome + '</td> <td>' + 
-                                                        item.nomeAgencia + '</td> <td>' + 
-                                                        item.nomeInstituicao + '</td> </tr>'
-                );
-            });
-            */ 
+            var totalPages = data['totalPages'];
+            var listaNumeros = [];
+            for(x=0; x< totalPages; x++){
+                listaNumeros[x] = {
+                    "numero":x + 1
+                };
+            }
+            var obj = {
+                paginas:listaNumeros
+            }
+
+            //console.log(paginas);
+            console.log(obj);
+
+            var rendered = Mustache.render(mustacheTemplatePaginacao, obj);
+            $('#paginacao').html(rendered);
         }
     });
 }
